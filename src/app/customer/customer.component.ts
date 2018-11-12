bfd123fdb3ac698ad7702625484d529d2fa15a04
Charles Vandyck <cjojovandyck@gmail.com>
2018-11-12 09:22:12 +0000

    first commit

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Customer }    from '../customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent {
+  nationalities =['','Afghanistan','Albana','Algeria','Andorra','Angola','Anguilla','Antigua','Armenia'];
+	
+
+  customer = new Customer(1, '', '', 23);
+
+  submitted = false;
+
+  onSubmit() { 
+    this.submitted = true; 
+  }
+}
